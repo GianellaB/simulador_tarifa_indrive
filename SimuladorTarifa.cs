b7d325f52cc06f441a9f5f05c2dd7b669657b444
GianellaB <gianellablasruiz@gmail.com>
2026-06-16 23:46:23 -0500

    feat: modificado

--- SimuladorTarifa.cs
+++ SimuladorTarifa.cs
@@ -2,27 +2,33 @@ public class SimuladorTarifa
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("=====================================");
-        Console.WriteLine("        InDrive - Simulador de Tarifa");
-        Console.WriteLine("=====================================");
-        //Entrada de Datos
-        Console.WriteLine("Nombre del Pasajero");
-        string nombre = Console.ReadLine();
+        Console.WriteLine("===============================================");
+        Console.WriteLine("         InDrive - Simulador de Tarifa");
+        Console.WriteLine("===============================================");
+        // Entrada de Datos
+        // Delcracion de variables
+        string nombre, nombreVehiculo;
+        double distancia, tarifaBase, costoKm, subtotal , tarifaFinal, descuento= 0.00;
+        int hora, tipoVehiculo;
+        bool esHoraPico = false;
 
-        Console.WriteLine("Ingrese distancia del viaje (Km): ");
-        double distancia = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Hora de salida (0 hrs - 23 hrs): ");
-        int hora = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("\nTipo de vehiculos: ");
-        Console.WriteLine("1. Economico ");
-        Console.WriteLine("2. Confort ");
-        Console.WriteLine("3. Premium ");
-        Console.WriteLine("4. Moto ");
-        Console.WriteLine("Ingrese la opcion:  ");
-        int tipoVehiculo = int.Parse(Console.ReadLine());
-        //proceso
+        Console.WriteLine("Nombre del Pasajero: ");
+        nombre = Console.ReadLine();
+        
+        Console.WriteLine("Ingrese distancia del viaje (KM): ");
+        distancia = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Hora de Salida (0 hrs - 23 hrs): ");
+        hora = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("\nTipo de Vehículo: ");
+        Console.WriteLine(" 1. Economico");
+        Console.WriteLine(" 2. Confort");
+        Console.WriteLine(" 3. Premium");
+        Console.WriteLine(" 4. Moto");
+        Console.WriteLine("Ingrese la opcion: ");
+        tipoVehiculo = int.Parse(Console.ReadLine());
+        // Proceso
         switch (tipoVehiculo){
             case 1: // Economico
                 nombreVehiculo = "Económico";
